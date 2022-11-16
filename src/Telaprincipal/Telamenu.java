b1df3843796c2a9a86b9f77cdc89a65fe8a2fa18
Na1au <analaura-osilva@hotmail.com>
2022-11-16 10:21:44 -0300

    init git

---
+++ src/Telaprincipal/Telamenu.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Telaprincipal;
+
+
+
+public class Telamenu {
+    public static void Telaprincipal(String[] args){
+    TelainicialUP menu = new TelainicialUP();
+    menu.setVisible(true);
+    
+    }
+}
---
+++ src/ac2/Registro.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ac2;
+
+
+public class Registro {
+    
+    private String nome = "";
+    private String email = "";
+    private String telefone = "";
+    private String cpf = "";
+    private String cidade = "";
+    private String estado = "";
+    
+    public Registro (){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    public void limpaCliente(){
+       nome = "";
+       cpf = "";
+       telefone = "";
+       email = "";
+       cidade = "";
+       estado = "";
+    }   
+    
+}
